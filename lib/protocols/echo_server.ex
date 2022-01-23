5331c101e43b1fb75aa8d53849cad95358ead3b4
carlosfritz <carlosfritz@posteo.net>
2022-01-23 19:05:05 +0100

    minor nit-pick

--- lib/protocols/echo_server.ex
+++ lib/protocols/echo_server.ex
@@ -11,15 +11,15 @@ defmodule ElixirRanch.Protocols.EchoServer do
 
   @impl true
   def init({ref, transport, _opts}) do
-	  {:ok, socket} = :ranch.handshake(ref)
-    :ok = transport.setopts(socket, [{:active, :once}])
+    {:ok, socket} = :ranch.handshake(ref)
+    :ok = transport.setopts(socket, [active: :once])
     :gen_server.enter_loop(__MODULE__, [], {socket, transport}, @timeout)
   end
 
   @impl true
   def handle_info({:tcp, socket, data}, {socket, transport} = state) do
     :ok = transport.send(socket, data)
-    :ok = transport.setopts(socket, [{:active, :once}])
+    :ok = transport.setopts(socket, [active: :once])
     {:noreply, state, @timeout}
   end
 
